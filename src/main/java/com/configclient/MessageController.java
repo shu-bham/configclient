24697e2ec5f165d2417f630681b7c90ecdef68ef
sbagrec1 <shubhamcs@optum.com>
2021-03-09 20:25:00 +0530

    add vault config

--- src/main/java/com/configclient/MessageController.java
+++ src/main/java/com/configclient/MessageController.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RefreshScope
 @RestController
 class MessageController {
@@ -12,8 +14,15 @@ class MessageController {
     @Value("${message:default message, client server not invoked}")
     private String message;
 
+    @Value("${vaultMessage:default vault message, client server not invoked}")
+    private String vaultMessage;
+
     @GetMapping("/message")
     public String getMessage() {
-        return this.message;
+
+        Map<String, String> stringMap = Map.of(
+                "message", this.message,
+                "vaultMessage", this.vaultMessage);
+        return stringMap.toString();
     }
 }
